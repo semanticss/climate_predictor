053bd637e3a3312ced00fdf466480588266d465f
hudson <huds0nben2007@gmail.com>
2025-04-25 09:24:30 -0500

    can pull all data i think idk how json works holy cow

--- src/visualization.py
+++ src/visualization.py
@@ -13,7 +13,6 @@ import json
 with open("../src/county_disaster_counts_by_year.json", "r") as f:
     disaster_data = json.load(f)
 
-
 records = []
 for geoid, data in disaster_data.items():
     total = sum(data["disasters_by_year"].values())
@@ -26,14 +25,12 @@ for geoid, data in disaster_data.items():
 
     disaster_df = pd.DataFrame(records)
 
-
 shapefile_path = '../data/raw/tl_2024_us_county/tl_2024_us_county.shp'
 counties = gpd.read_file(shapefile_path)
 counties = counties.to_crs(epsg=4326) # idk what this even means but everything breaks without it
 countries_GEOID = counties['GEOID']
 
 counties = counties.merge(disaster_df, on="GEOID", how="left")
-
 
 fig, ax = plt.subplots(figsize=(15, 10), subplot_kw={"projection": ccrs.PlateCarree()})
 counties.plot(column="total_disasters", cmap="Reds", linewidth=0.1, edgecolor="black", ax=ax, legend=True)
